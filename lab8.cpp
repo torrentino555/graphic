13bc4fc2263367a00c4350188d6a1d72638f810c
Vlad Orlov <orl_vlad@mail.ru>
2017-09-01 04:09:25 +0300

    rename

---
+++ lab8.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Shader.h"
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 800;
+GLfloat spin_x = 0.0f, spin_y = 0.0f, spin_z = 0.0f, scale = 1.0f, x = 0.0f, y = 0.0f, z = 0.0f, timer = glfwGetTime();
+GLboolean moving = false;
+
+void MoveObject() {
+  spin_x += 2.0f * (glfwGetTime() - timer);
+  spin_y += 2.0f * (glfwGetTime() - timer);
+  spin_z += 2.0f * (glfwGetTime() - timer);
+  timer = glfwGetTime();
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glEnable(GL_DEPTH_TEST);
+
+    Shader ourShader("shader.vs", "shader.frag");
+
+
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (moving)
+          MoveObject();
+
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ourShader.Use();
+
+        // Create transformations
+        glm::mat4 transform;
+        transform = glm::translate(transform, glm::vec3(x, y, z));
+        transform = glm::rotate(transform, -spin_x, glm::vec3(1.0f, 0.0f, 0.0f));
+        transform = glm::rotate(transform, -spin_y, glm::vec3(0.0f, 1.0f, 0.0f));
+        transform = glm::rotate(transform, -spin_z, glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::scale(transform, glm::vec3(scale, scale, scale));
+
+        GLint transformLoc = glGetUniformLocation(ourShader.Program, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glfwSwapBuffers(window);
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  else if (key == GLFW_KEY_M && action == GLFW_PRESS)
+    moving = !moving;
+  else if (key == GLFW_KEY_S && action != GLFW_RELEASE)
+    spin_x += 0.1f;
+  else if (key == GLFW_KEY_W && action != GLFW_RELEASE)
+    spin_x -= 0.1f;
+  else if (key == GLFW_KEY_D && action != GLFW_RELEASE)
+    spin_y += 0.1f;
+  else if (key == GLFW_KEY_A && action != GLFW_RELEASE)
+    spin_y -= 0.1f;
+  else if (key == GLFW_KEY_E && action != GLFW_RELEASE)
+    spin_z += 0.1f;
+  else if (key == GLFW_KEY_Q && action != GLFW_RELEASE)
+    spin_z -= 0.1f;
+
+  else if (key == GLFW_KEY_EQUAL && action != GLFW_RELEASE)
+    scale += 0.1f;
+  else if (key == GLFW_KEY_MINUS && action != GLFW_RELEASE)
+    scale -= 0.1f;
+
+  else if (key == GLFW_KEY_LEFT && action != GLFW_RELEASE)
+    x -= 0.1;
+  else if (key == GLFW_KEY_RIGHT && action != GLFW_RELEASE)
+    x += 0.1;
+  else if (key == GLFW_KEY_DOWN && action != GLFW_RELEASE)
+    z += 0.1;
+  else if (key == GLFW_KEY_UP && action != GLFW_RELEASE)
+    z -= 0.1;
+  else if (key == GLFW_KEY_Z && action != GLFW_RELEASE)
+    y += 0.1;
+  else if (key == GLFW_KEY_X && action != GLFW_RELEASE)
+    y -= 0.1;
+}
